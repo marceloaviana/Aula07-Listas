f4356b11b9afe620f17d8d39fefb1cebe85746ef
marceloaviana <96154514+marceloaviana@users.noreply.github.com>
2023-11-08 16:23:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# Exercico 19
+
+
+lista_numeros = [3, 7, 1, 4, 9, 2, 10, 4]
+
+soma = 0
+
+for numero in lista_numeros:
+    soma += numero
+
+print(f"A soma dos elementos da lista é: {soma}")
+
+
+print("---------------------------------")
+# Exercicio 20
+
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+soma_pares = 0
+soma_impares = 0
+
+for numero in numeros:
+  if numero % 2 == 0:
+    soma_pares += numero
+  elif numero % 2 != 0:
+    soma_impares += numero
+
+print(f"A soma dos valores pares é: {soma_pares} e a soma dos valores impares é: {soma_impares}")
+
+
